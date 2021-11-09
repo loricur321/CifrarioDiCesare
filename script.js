4014f082a56024d4c183ae48075e28a67ef665e3
loricur321 <loricur@gmail.com>
2021-11-09 18:35:13 +0100

    Prima versione

---
+++ script.js
@@ -0,0 +1,56 @@
+var alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toUpperCase().split("");
+
+function Cifra(){
+    var fraseDaCifrare = document.getElementById("fraseDaCifrare").value.toUpperCase().split("");
+    var chiave = parseInt(document.getElementById("chiaveCifratura").value);
+    var bitFrase = new Array(fraseDaCifrare.length);
+
+    if(chiave <= 0 || chiave > 25)
+    {
+        alert("La chiave di cifratura deve essere compresa tra 1 e 25!")
+        chiave = 1;
+    }
+
+    for(let i = 0; i < fraseDaCifrare.length; i++)
+    {
+        bitFrase[i] = fraseDaCifrare[i].charCodeAt(0);
+
+        if(bitFrase[i] < 65 || bitFrase[i] > 90)
+        {
+            alert("La frase deve essere composta da lettere dell'alfabeto!")
+            break;
+        }
+    }
+
+    var contatore; //calcolo le lettere in più in caso il codice superi l'ascii code della lettera Z
+
+    for(let i = 0; i < fraseDaCifrare.length; i++)
+    {
+        contatore = 0;
+
+        bitFrase[i] = bitFrase[i] + chiave;
+
+        if(bitFrase[i] > 90) //90 è Z
+        {
+            contatore = bitFrase[i] - 90;
+            bitFrase[i] = alfabeto[contatore - 1].charCodeAt(0);
+        }
+    }
+
+    var result = RicomponiStringa(bitFrase);
+    document.getElementById("risultatoCifratura").innerHTML = result;
+}
+
+function RicomponiStringa(bitFrase){
+    let retVal = "";
+
+    bitFrase.forEach(bit => {
+        for(let i = 65; i <= 90; i++)
+        {
+            if(bit == i)
+                retVal += alfabeto[i - 65];
+        }
+    });
+
+    return retVal;
+}
